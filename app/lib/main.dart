368fe58733ed3d296d11f25855aaec8fab1bc44e
TiaanBosman101 <bosmantiaan22@gmail.com>
2025-05-22 16:49:00 +0200

    Added package that sets minimmum window size

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:app/shell.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = WindowOptions(
+    minimumSize: Size(900, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
