ca3789c55b29589b6e2a1784b331f409e1562c0e
TiaanBosman101 <bosmantiaan22@gmail.com>
2025-05-23 14:31:27 +0200

    initial manager page

--- app/lib/pages/manager_page.dart
+++ app/lib/pages/manager_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class ManagerPage extends StatelessWidget {
   final String name;
@@ -16,6 +17,41 @@ class ManagerPage extends StatelessWidget {
           fontSize: 20,
         ),
         shape: Border(bottom: BorderSide(color: Color(0xff3D3D3D), width: 1)),
+        actions: [
+          Row(
+            children: [
+              Expanded(
+                flex: 3,
+                child: Container(
+                  width: 100,
+                  height: 20,
+                  padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xff094E3A),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '75% organized',
+                      style: TextStyle(fontSize: 10, color: Color(0xff6EE79B)),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ToggleSwitch(
+                  initialLabelIndex: 0,
+                  totalSwitches: 2,
+                  labels: ['Folder View', 'Graph View'],
+                  onToggle: (index) {
+                    print('switched to: $index');
+                  },
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
