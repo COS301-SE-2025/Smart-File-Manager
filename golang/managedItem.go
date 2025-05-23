c4b6bd1d1afd1e34321ba38755a7320d37650f7a
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-05-23 12:03:59 +0200

    Composite implementation complete, moving into testing now

--- golang/managedItem.go
+++ golang/managedItem.go
@@ -1,43 +1,72 @@
 package main
 
-import "time"
+import (
+	"time"
+)
 
+// tag structure
+type tag struct {
+	tagID   string
+	tagName string
+}
+
+// component interface
+type FileSystemItem interface {
+	GetPath() string
+	RemoveItem(itemPath string) bool
+}
+
+// base struct
 type managedItem struct {
 	itemID       string
 	itemName     string
-	itemPath     string // Use string for file paths; there is no built-in 'filepath' type
+	itemPath     string
 	itemTags     []tag
 	locked       bool
 	fileType     string
 	creationDate time.Time
 }
 
-//composite functions
-// func (m managedItem) createBackup() memento {
-
-// }
-// func (m managedItem) createBackup(savedBackup memento) {
-
-// }
-
-type Folder struct {
-	managedItem
-	containedItems []managedItem
+func (m *managedItem) GetPath() string {
+	return m.itemPath
 }
 
-//component functions
-// func (f *Folder) addItem(newItem managedItem) {
-// }
-
-// func (f *Folder) removeItem(existingItem managedItem) bool {
-
-// }
-
+// Leaf
 type File struct {
 	managedItem
 }
 
-type tag struct {
-	tagID   string
-	tagName string
+func (f *File) RemoveItem(itemPath string) bool {
+	// A file has no children; return false
+	return false
+}
+
+// Composite
+type Folder struct {
+	managedItem
+	containedItems []FileSystemItem
+}
+
+func (f *Folder) AddItem(newItem FileSystemItem) {
+	f.containedItems = append(f.containedItems, newItem)
+}
+
+func (f *Folder) RemoveItem(itemPath string) bool {
+	for i, item := range f.containedItems {
+		if item.GetPath() == itemPath {
+			f.containedItems = append(f.containedItems[:i], f.containedItems[i+1:]...)
+			return true
+		}
+		// if item is a Folder, attempt recursive removal
+		if folder, ok := item.(*Folder); ok {
+			if folder.RemoveItem(itemPath) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (f *Folder) GetPath() string {
+	return f.itemPath
 }
