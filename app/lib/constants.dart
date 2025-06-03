ab3b94773c629c89731431a0c59b1b31d59a13c0
TiaanBosman101 <bosmantiaan22@gmail.com>
2025-06-03 16:57:18 +0200

    added costants file for styling of text and colors

---
+++ app/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kprimaryColor = Color(0xffFFB400);
+const kScaffoldColor = Color(0xff1E1E1E);
+const kAppBarColor = Color(0xff2E2E2E);
+const kOutlineBorder = Color(0xff3D3D3D);
+const kYellowText = Color(0xffFFB400);
+
+const kTitle1 = TextStyle(
+  color: Color(0xffFFB400),
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+);
