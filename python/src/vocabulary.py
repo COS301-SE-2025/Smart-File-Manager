b5c5d7b1194fcb0c6db832d716276a29a250391c
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-06-16 15:24:01 +0200

    Create vocabulary of all seen keywords

--- python/src/vocabulary.py
+++ python/src/vocabulary.py
@@ -3,12 +3,25 @@
 class Vocabulary:
     def __init__(self):
         self.vocab = []
+        self.seen = set()
 
     def createVocab(self, keywords):
+        self.seen = set()
+        self.vocab = []
         for filename, keywords in keywords.items():
             # print(f"\n== FILE: {filename} ==")
             for kw, score in keywords:
-                self.vocab.append(kw)
-                
-    def getVocab(self):
+                if kw not in self.seen:
+                    self.seen.add(kw)
+                    self.vocab.append(kw)
+        return self.vocab
+    
+    def addToVocab(self, keywords):
+        vocab = []
+        for filename, kw_list in keywords.items():
+            for kw, score in kw_list:
+                if kw not in self.seen:
+                    self.seen.add(kw)
+                    vocab.append(kw)
+        self.vocab.append(vocab)
         return self.vocab
