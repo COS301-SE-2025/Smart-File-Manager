7678ddce2f27bc1beb378d38eaf7c1436f501631
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-06-16 15:24:19 +0200

    keyword cluster creation

---
+++ python/src/create_kw_cluster.py
@@ -0,0 +1,9 @@
+class KWCluster:
+
+    def createCluster(self, keywords, vocab):
+        kw_score_map = {kw: score for kw, score in keywords}
+        cluster_vector = []
+        for kw in vocab:
+            cluster_vector.append(kw_score_map.get(kw, 0))  # score if exists else 0
+
+        return cluster_vector
--- python/src/full_vector.py
+++ python/src/full_vector.py
@@ -1,8 +1,36 @@
-
+from create_kw_cluster import KWCluster
 
 class FullVector:
     def __init__(self):
         self.full_vector = []
 
+    def createFullVector(self):
+        pass
+        # assign_IDF         
+        # one hot encoding 
+        # 
+        # for all files:
+        #   self.full.vec.append(clustervec[index], filetype[index], size[index])
+    #helper function
+    def assignTF_IDF(self, result, vocabKW):
+        # process request root
+        # Return vector of TF-IDF
+        # Compare direct words, similiar words (cat - cats), contained words (AI - AI-driven)
+        # Assign TF-IDF score
+        # returns:
+        # [
+        # [0,0,0,0.51,0,0.1],
+        # [...]
+        # ]
+        kwclust = KWCluster()
+        clusterVec = []
+        # for kw in vocabKW:
+        #     print(kw)
+        for filename, keywords in result.items():
+            kwcluster = kwclust.createCluster(keywords,vocabKW)
+            clusterVec.append(kwcluster)
+            #print(clusterVec)
+
+        return clusterVec
 
 
