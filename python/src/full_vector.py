28b8e4b5544437936ca005c181769d7d0add2f2f
amJohnnyma <dewaldcoleskydc@gmail.com>
2025-06-18 12:21:08 +0200

    start conversion to use a map of files

--- python/src/full_vector.py
+++ python/src/full_vector.py
@@ -1,47 +1,41 @@
 from create_kw_cluster import KWCluster
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 class FullVector:
     def __init__(self):
         self.full_vector = []
-        self.tfidf_vector = []
-        self.filetypes = []
-        self.sizes = []
-        self.encoded_filetypes = []
+        self.seen_filetypes = []
 
-    def createFullVector(self, keywords, vocab, root):
-        self.tfidf_vec = self.assignTF_IDF(keywords,vocab)
-        self.filetypes, self.sizes = self.assignFileTypeAndSize(root)
-        self.encoded_filetypes = self.oneHotEncoding(self.filetypes)
+#File map [
+#     {
+#         "filename" : "name",
+#         "keywords" : [],
+#         "size_kb" : int,
+#         "filetype" : "type",
+#         # ADD
+#         "full_vector": [["keywords"], ["filetype"], "size"]
+#     },
+#     ...
+# ]
+    def createFullVector(self, files, vocabKW, filetypes, sizes):
+        for file in files:
+            self.full_vector = []
+            tfidf_vec = self.assignTF_IDF(file["keywords"],vocabKW)            
+            encoded_filetype = self.oneHotEncoding(file["filetype"], filetypes)        
+            scaler = MinMaxScaler() #############################      
+            scaler.fit_transform(np.array(sizes))
+            file["full_vector"] = tfidf_vec
 
-        # Catch bad data
-        if len(self.tfidf_vec) != len(self.encoded_filetypes):
-            return []
-        if len(self.encoded_filetypes) != len(self.sizes):
-            return []
 
-        # Loop through each files
-        for i in range(len(self.tfidf_vec)):
-            self.full_vector.append((self.tfidf_vec[i], self.encoded_filetypes[i], self.sizes[i]))
-        
-        # for entry in self.full_vector:
-        #     print("\n")
-        #     print(entry)
-
-        combined_points = []
-        # combine into a single tuple
-        for vec in self.full_vector:
-            combined_points.append(list(vec[0]) + list(vec[1]) + [vec[2]])
-
-        return combined_points
 
     #helper function
     def assignTF_IDF(self, result, vocabKW):
 
         kwclust = KWCluster()
         tfidf = []
-
         
         for filename, keywords in result.items():
             kwcluster = kwclust.createCluster(keywords,vocabKW)
@@ -57,20 +51,16 @@ class FullVector:
             # try catch incase conversion fails? -> add fixed data then?
             file_size = next((int(entry.value) for entry in file.metadata if entry.key == "size_bytes"), None)
             types.append(file_type)
-            sizes.append(file_size)
+            sizes.append(file_size)            
         return types, sizes
     
-    def oneHotEncoding(self, filetypes):
+
+    def oneHotEncoding(self, filetype, filetypes):
         df = pd.DataFrame(filetypes, columns=["filetype"])
-
         encoder = OneHotEncoder(sparse_output=False)
-
         one_hot_encoded = encoder.fit_transform(df[["filetype"]])
-
         one_hot_df = pd.DataFrame(one_hot_encoded, columns=encoder.get_feature_names_out(["filetype"]))
-
         # print(f"encoded data: \n{one_hot_df}")
-
         return one_hot_df.values.tolist()
 
 
