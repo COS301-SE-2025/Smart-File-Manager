9147341cedf9815e833769faa6cab06c5bff095f
Jack-jack1 <jackbawden01@gmail.com>
2025-06-20 10:15:08 +0200

    implemented loadTree: recursively builds json response

--- golang/filesystem/server.go
+++ golang/filesystem/server.go
@@ -1,6 +1,7 @@
 package filesystem
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"slices"
@@ -8,6 +9,7 @@ import (
 )
 
 var (
+	//array of smartfile managers
 	composites []*Folder
 	mu         sync.Mutex
 )
@@ -71,12 +73,103 @@ func addTagHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("false"))
 }
 
+// struct for json return type for 200 reqs
+type directoryTreeJson struct {
+	Name     string     `json:"name"`
+	IsFolder bool       `json:"isFolder"`
+	Children []FileNode `json:"children"`
+}
+
+// file or folder
+type FileNode struct {
+	Name     string     `json:"name"`
+	Path     string     `json:"path,omitempty"`
+	IsFolder bool       `json:"isFolder"`
+	Tags     []string   `json:"tags,omitempty"`
+	Metadata *Metadata  `json:"metadata,omitempty"`
+	Children []FileNode `json:"children,omitempty"`
+}
+
+type Metadata struct {
+	Size         string `json:"size"`
+	DateCreated  string `json:"dateCreated"`
+	Owner        string `json:"owner"`
+	LastModified string `json:"lastModified"`
+}
+
+func loadTreeDataHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	var found bool = false
+
+	for _, c := range composites {
+		fmt.Println(c.Name)
+		if c.Name == name {
+			found = true
+			structureChildren := createDirectoryJsonStructure(c)
+			root := directoryTreeJson{
+				Name:     c.Name,
+				IsFolder: true,
+				Children: structureChildren,
+			}
+
+			if err := json.NewEncoder(w).Encode(root); err != nil {
+				http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			}
+			break
+		}
+	}
+	if !found {
+		//400 code
+		http.Error(w, "No smart manager with that name", http.StatusBadRequest)
+	}
+}
+
+func createDirectoryJsonStructure(root *Folder) []FileNode {
+	fmt.Print("exploring " + root.Name)
+	FileNodeStructure := FileNode{}
+
+	for _, file := range root.Files {
+		curr := FileNode{
+			Name:     file.Name,
+			Path:     file.Path,
+			IsFolder: false,
+			Tags:     file.Tags,
+			Metadata: &Metadata{},
+		}
+		FileNodeStructure.Children = append(FileNodeStructure.Children, curr)
+	}
+	for _, folder := range root.Subfolders {
+		curr := FileNode{
+			Name:     folder.Name,
+			Path:     folder.Path,
+			IsFolder: false,
+			Tags:     folder.Tags,
+			Metadata: &Metadata{},
+		}
+		FileNodeStructure.Children = append(FileNodeStructure.Children, curr)
+		createDirectoryJsonStructure(folder)
+	}
+	return FileNodeStructure.Children
+
+}
+
 func HandleRequests() {
+
+	// path, _ := os.Getwd()
+	// fmt.Println("THE PATH: " + path)
+	// path = filepath.Dir(path)
+	// path = filepath.Join(path, "python/testing")
+	// fmt.Println("THE PATH: " + path)
+
 	http.HandleFunc("/addDirectory", getCompositeHandler)
 	http.HandleFunc("/removeDirectory", removeCompositeHandler)
 	http.HandleFunc("/addTag", addTagHandler)
+	http.HandleFunc("/loadTreeData", loadTreeDataHandler)
 	fmt.Println("Server started on port 51000")
-	http.ListenAndServe(":51000", nil)
+	// http.ListenAndServe(":51000", nil)
+	http.ListenAndServe("0.0.0.0:51000", nil)
+
 }
 
 // Getter for main.go
