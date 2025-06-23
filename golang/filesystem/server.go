981970d468f1b8863ed060c14cb716ede77e2dae
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-06-23 09:31:35 +0200

    Updated API documentation file

--- golang/filesystem/server.go
+++ golang/filesystem/server.go
@@ -117,7 +117,7 @@ func HandleRequests() {
 	http.HandleFunc("/removeDirectory", removeCompositeHandler)
 	http.HandleFunc("/addTag", addTagHandler)
 	http.HandleFunc("/removeTag", removeTagHandler)
-	// http.HandleFunc("/loadTreeData", loadTreeDataHandler)
+	http.HandleFunc("/loadTreeData", loadTreeDataHandler)
 	fmt.Println("Server started on port 51000")
 	// http.ListenAndServe(":51000", nil)
 	http.ListenAndServe("0.0.0.0:51000", nil)
