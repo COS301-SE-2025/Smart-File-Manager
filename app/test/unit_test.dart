428120342cfe5ed3b7db4e09281d660d39583161
phillDup <philippdup@gmail.com>
2025-06-24 05:52:50 +0200

    Changed from unit_tests to unit_test

--- app/test/unit_test.dart
+++ app/test/unit_test.dart
@@ -8,6 +8,7 @@ import 'package:app/custom_widgets/create_manager.dart';
 import 'package:app/navigation/main_navigation.dart';
 
 //flutter packages pub run build_runner build
+// Added this comment cause I changed the file name and the commit does not work otherwise
 @GenerateMocks([http.Client])
 void main() {
   group('FileTreeNode Tests', () {
