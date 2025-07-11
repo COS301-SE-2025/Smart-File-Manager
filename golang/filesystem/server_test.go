6411c10cd80c2f607613c69773fce5c84332708b
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-07-11 16:14:48 +0200

    Added tests for the API endpoints that lock and unlock files and folders

--- golang/filesystem/server_test.go
+++ golang/filesystem/server_test.go
@@ -43,3 +43,31 @@ func TestAPI_AddAndRemoveTag(t *testing.T) {
 		t.Fatalf("removeTagHandler: expected true, got %s", w.Body.String())
 	}
 }
+
+func TestAPI_LockUnlock(t *testing.T) {
+	// Add composite
+	req := httptest.NewRequest("GET", "/addDirectory?name=testlock&path=./testRootFolder", nil)
+	w := httptest.NewRecorder()
+	addCompositeHandler(w, req)
+
+	// Lock
+	req = httptest.NewRequest("GET", "/lock?path=./testRootFolder", nil)
+	w = httptest.NewRecorder()
+	lockHandler(w, req)
+	if w.Body.String() != "true" {
+		t.Fatalf("lockHandler: expected true, got %s", w.Body.String())
+	}
+
+	// Unlock
+	req = httptest.NewRequest("GET", "/unlock?path=./testRootFolder", nil)
+	w = httptest.NewRecorder()
+	unlockHandler(w, req)
+	if w.Body.String() != "true" {
+		t.Fatalf("unlockHandler: expected true, got %s", w.Body.String())
+	}
+
+	// Cleanup
+	req = httptest.NewRequest("GET", "/removeDirectory?path=./testRootFolder", nil)
+	w = httptest.NewRecorder()
+	removeCompositeHandler(w, req)
+}
