2b43b01675281b1a5c7ea2e8a1b97cc767319dc8
Jack-jack1 <jackbawden01@gmail.com>
2025-07-25 14:25:14 +0200

    concurrent search (returns duplicates)

--- golang/filesystem/handler_search.go
+++ golang/filesystem/handler_search.go
@@ -1,12 +1,17 @@
 package filesystem
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
-	"strconv"
+	"sync"
 )
 
-const limit int = 20
+//todo
+// remove file extention from name searchText
+// take length into acount. ie searching "jacks" should find "jacks books" before "bills"
+
+const limit int = 15
+const maxDist int = 25
 
 func levelshteinDist(a, b string) int {
 	la, lb := len(a), len(b)
@@ -55,27 +60,7 @@ func levelshteinDist(a, b string) int {
 	return prev[lb]
 }
 
-func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	compositeName := r.URL.Query().Get("compositeName")
-	searchText := r.URL.Query().Get("searchText")
-
-	fmt.Println(strconv.Itoa(levelshteinDist(searchText, "jacks records")))
-
-	for _, comp := range composites {
-		if comp.Name == compositeName {
-			fmt.Println("entered")
-			results := getMatches(searchText, comp)
-			for _, i := range results.rankedFiles {
-				fmt.Println(i.file.Name)
-				fmt.Println(i.distance)
-			}
-		}
-	}
-
-}
-
 type safeResults struct {
-	// mu          sync.RWMutex
 	Name        string
 	rankedFiles []rankedFile
 }
@@ -85,54 +70,105 @@ type rankedFile struct {
 	distance int
 }
 
+type folderResponse struct {
+	Name  string `json:"name"`
+	Files []File `json:"files"`
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	compositeName := r.URL.Query().Get("compositeName")
+	searchText := r.URL.Query().Get("searchText")
+
+	for _, comp := range composites {
+		if comp.Name == compositeName {
+
+			sr := getMatches(searchText, comp)
+
+			// build a FolderResponse with just the File objects
+			resp := folderResponse{
+				Name:  sr.Name,
+				Files: make([]File, len(sr.rankedFiles)),
+			}
+			for i, rf := range sr.rankedFiles {
+				resp.Files[i] = rf.file
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(resp)
+			return
+		}
+	}
+
+}
+
 func getMatches(text string, composite *Folder) *safeResults {
 	res := &safeResults{
 		Name: composite.Name,
 	}
 
-	for _, file := range composite.Files {
-		var dist int = levelshteinDist(file.Name, text)
-		if dist <= 15 {
-			currentRankedFile := &rankedFile{
-				file:     *file,
-				distance: dist,
-			}
+	resultChan := make(chan rankedFile)
 
-			inserted := false
-			for i, iteratedRankedFile := range res.rankedFiles {
-				//if current is better
-				if iteratedRankedFile.distance > currentRankedFile.distance {
+	var wg sync.WaitGroup
 
-					if len(res.rankedFiles) < limit {
-						//insert by shifting over to make the array sorted
-						res.rankedFiles = append(
-							append(res.rankedFiles[:i], *currentRankedFile),
-							res.rankedFiles[i:]...,
-						)
+	wg.Add(1)
+	go exploreFolder(composite, text, resultChan, &wg)
 
-						inserted = true
-						break
-					} else {
-						res.rankedFiles = append(
-							append(
-								res.rankedFiles[:i],
-								*currentRankedFile,
-							),
-							res.rankedFiles[i:limit-1]...,
-						)
-						inserted = true
-						break
-					}
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	for currentRankedFile := range resultChan {
+
+		inserted := false
+		for i, iteratedRankedFile := range res.rankedFiles {
+			//if current is better
+			if iteratedRankedFile.distance > currentRankedFile.distance {
+
+				if len(res.rankedFiles) < limit {
+					//insert by shifting over to make the array sorted
+					res.rankedFiles = append(
+						append(res.rankedFiles[:i], currentRankedFile),
+						res.rankedFiles[i:]...,
+					)
+
+					inserted = true
+					break
+				} else {
+					res.rankedFiles = append(
+						append(
+							res.rankedFiles[:i],
+							currentRankedFile,
+						),
+						res.rankedFiles[i:limit-1]...,
+					)
+					inserted = true
+					break
 				}
 			}
-			// if limit is not reached and not inserted then we insert at the end
-			if len(res.rankedFiles) < limit {
-				if !inserted {
-					res.rankedFiles = append(res.rankedFiles, *currentRankedFile)
-				}
+		}
+		// if limit is not reached and not inserted then we insert at the end
+		if len(res.rankedFiles) < limit {
+			if !inserted {
+				res.rankedFiles = append(res.rankedFiles, currentRankedFile)
 			}
-
 		}
 	}
+
 	return res
+}
+
+func exploreFolder(f *Folder, text string, c chan<- rankedFile, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for _, folder := range f.Subfolders {
+		wg.Add(1)
+		go exploreFolder(folder, text, c, wg)
+	}
+	for _, file := range f.Files {
+		dist := levelshteinDist(file.Name, text)
+		if dist <= maxDist {
+
+			c <- rankedFile{file: *file, distance: dist}
+		}
+	}
 }
