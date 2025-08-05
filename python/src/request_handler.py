cfe776dc03b3d46704c57c52c1627598ae9e2e4e
phillDup <philippdup@gmail.com>
2025-08-05 16:30:09 +0200

    Debug

--- python/src/request_handler.py
+++ python/src/request_handler.py
@@ -8,13 +8,12 @@ import master
 # Class for handling requests to gRPC python server
 # Assigns request to master for processing (currently assigns each request to a single master)
 
-transformer = SentenceTransformer('all-MiniLM-L6-v2', local_files_only=True)
 class RequestHandler(message_structure_pb2_grpc.DirectoryServiceServicer):
 
     def __init__(self):
         # Early initialize sentence transformer
-
-        self.master = master.Master(1, transformer)
+        transformer = SentenceTransformer('all-MiniLM-L6-v2', local_files_only=True)
+        self.master = master.Master(10, transformer)
 
 
     def SendDirectoryStructure(self, request, context):
@@ -24,7 +23,7 @@ class RequestHandler(message_structure_pb2_grpc.DirectoryServiceServicer):
     def serve(self):
         port = "50051"
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        message_structure_pb2_grpc.add_DirectoryServiceServicer_to_server(RequestHandler(), server)
+        message_structure_pb2_grpc.add_DirectoryServiceServicer_to_server(self, server)
         server.add_insecure_port("[::]:" + port)
         server.start()
         print("Server started, listening on " + port)
