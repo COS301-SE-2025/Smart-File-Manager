e9bea162a38241d322dfa7fa857e0830e7cd4ad2
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-08-12 14:35:39 +0200

    Added function to count number of files and folders in a manager

---
+++ golang/filesystem/statistics.go
@@ -0,0 +1,100 @@
+package filesystem
+
+/*
+	statistics we need
+	num managers
+	num files	& per manager
+	num folders & per manager
+	size of manager & per manager
+
+	Most recently used files	lim=5
+	largest files	lim=5
+	least recently used files 	lim=5
+
+
+	umbrella files perentage of all files
+
+	json structure:
+	// [
+	//	{
+	//	 	"manager_name": manager1,
+			"size": 1024,
+			"folders": 10,
+			"files": 100,
+			"recent": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"largest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"oldest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"umbrella_percentage": ["5%","20%","10%","20%","10%","20%","10%","15%"]
+	//	},
+	//	{
+	//	  	"manager_name": manager2,
+			"size": 1024,
+			"folders": 10,
+			"files": 100,
+			"recent": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"largest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"oldest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"umbrella_percentage": ["5%","20%","10%","20%","10%","20%","10%","15%"]
+	//	},
+	//	{
+	//	  	"manager_name": manager3,
+			"size": 1024,
+			"folders": 10,
+			"files": 100,
+			"recent": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"largest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"oldest": {
+						file_paths: ["/home/user/documents/report.pdf", "/home/user/photos/vacation.jpg", "/home/user/music/song.mp3"],
+						file_names: ["report.pdf", "vacation.jpg", "song.mp3"]
+						},
+			"umbrella_percentage": ["5%","20%","10%","20%","10%","20%","10%","15%"]
+	//	}
+	//
+
+// ]
+*/
+type file struct {
+	FilePath string `json:"file_path"`
+	FileName string `json:"file_name"`
+}
+type ManagerStatistics struct {
+	ManagerName        string   `json:"manager_name"`
+	Size               int      `json:"size"`
+	Folders            int      `json:"folders"`
+	Files              int      `json:"files"`
+	Recent             []file   `json:"recent"`
+	Largest            []file   `json:"largest"`
+	Oldest             []file   `json:"oldest"`
+	UmbrellaPercentage []string `json:"umbrella_percentage"`
+}
+
+func getNumItems(m *ManagerStatistics, item *Folder) {
+	m.Files += len(item.Files)
+	for _, subFolder := range item.Subfolders {
+		getNumItems(m, subFolder)
+	}
+}
