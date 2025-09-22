40945ffc4d5d28d2703e5dc10e1615e39fe5fe66
Jack-jack1 <jackbawden01@gmail.com>
2025-09-22 17:10:59 +0200

    saves from api successfully

---
+++ golang/filesystem/handler_setPreferredCase.go
@@ -0,0 +1,46 @@
+package filesystem
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func SetPreferredCase(w http.ResponseWriter, r *http.Request) {
+	preferredCase := r.URL.Query().Get("preferredCase")
+
+	err := storePreferredCase(preferredCase)
+	if err == nil {
+		jsonResponse := []byte(`{"responseMessage": "Saved preferredCase successfully"}`)
+		w.Write(jsonResponse)
+		return
+	}
+	http.Error(w, ("Failed to write to file: " + err.Error()), http.StatusInternalServerError)
+
+}
+
+func storePreferredCase(preferredCase string) error {
+	filePath := filepath.Join("storage", "preferredCase.txt")
+
+	
+
+	//create file
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("Error creating file: %v\n", err)
+		return err
+	}
+	defer file.Close()
+
+	data := []byte(preferredCase)
+
+	//write to file
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Printf("Error writing to file: %v\n", err)
+		return err
+	}
+
+	return nil
+}
