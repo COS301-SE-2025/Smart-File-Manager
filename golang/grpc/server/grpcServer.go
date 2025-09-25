9eb7a6bebd61ea87bdec134da5c30a5ef83559bf
NicohaasHerhaasus <hencopritor06@gmail.com>
2025-09-25 15:42:01 +0200

    updated python connection to use dynamic port

--- golang/grpc/server/grpcServer.go
+++ golang/grpc/server/grpcServer.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	pb "github.com/COS301-SE-2025/Smart-File-Manager/golang/client/protos"
 	"google.golang.org/grpc"
@@ -48,14 +49,22 @@ func (s *server) SendDirectoryStructure(ctx context.Context, in *pb.DirectoryReq
 }
 
 func main() {
-	fmt.Println("running go server")
-	lis, err := net.Listen("tcp", ":50051")
+	fmt.Println("Starting go server...")
+	// Read port from environment (default to 50051 if not set)
+	port := os.Getenv("PYTHON_SERVER")
+	if port == "" {
+		port = "50051"
+	}
+	addr := ":" + port
+
+	lis, err := net.Listen("tcp", addr)
 	if err != nil {
-		log.Fatalf("failed to listen on port 50051: %v", err)
+		log.Fatalf("failed to listen on port %s: %v", port, err)
 	}
 
 	s := grpc.NewServer()
 	pb.RegisterDirectoryServiceServer(s, &server{})
+
 	log.Printf("gRPC server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
